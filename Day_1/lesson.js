4973ac754c1cd9f6b0e2eeb2e43cc090a79827bd
Dev Kafayat <olubukola11th@gmail.com>
2024-01-25 13:32:11 +0100

    first commit

---
+++ Day_1/lesson.js
@@ -0,0 +1,51 @@
+// comments can make code readable
+// Welcome to 30 DaysofJavascript
+/* comments can make code readable,
+easy to reuse and informative */
+
+let thisString = 'Hey, there!';
+let bol = true;
+let undefinedVariable;
+let emptyValue = null;
+let answer = null
+console.log(thisString);
+console.log(bol);
+console.log(undefinedVariable);
+console.log(emptyValue)
+console.log(typeof thisString);
+console.log(typeof bol);
+console.log(typeof emptyValue);
+console.log(typeof undefinedVariable);
+console.log(typeof answer);
+
+let emptyList;
+let emptyString;
+let undefinedList;
+let undefinedDictionary;
+let _thisString = "How d'you do?";
+let number = 2000;
+let mystrength = 'Renewed';
+let bol_= "False";
+let myAge = "I am 23 years old";
+let yourAge = 'you are 34 years old';
+console.log(emptyList);
+console.log(emptyString);
+console.log(undefinedList);
+console.log(undefinedDictionary);
+console.log(_thisString);
+console.log(number);
+console.log(mystrength);
+console.log(bol_);
+console.log(myAge);
+console.log(yourAge);
+
+console.log(typeof emptyList);
+console.log(typeof emptyString);
+console.log(typeof undefinedList);
+console.log(typeof undefinedDictionary);
+console.log(typeof _thisString);
+console.log(typeof number);
+console.log(typeof mystrength);
+console.log(typeof bol_);
+console.log(typeof myAge);
+console.log(typeof yourAge);
